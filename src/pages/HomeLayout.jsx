e3dc594faf559de7ad23b305c916f60984becf61
radu-bordea <radu.bordea.dev@gmail.com>
2024-01-31 22:03:59 +0100

    create pages and implement react router

---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const HomeLayout = () => {
+  return (
+    <div>
+      <h1>HomeLayout</h1>
+      <Link to='/about'>About</Link>
+    </div>
+  );
+};
+
+export default HomeLayout;
