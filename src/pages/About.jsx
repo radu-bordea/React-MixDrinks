c3332c101d73c5d95cc91ac5ef0b8630d771f4ab
radu-bordea <radu.bordea.dev@gmail.com>
2024-02-04 22:31:27 +0100

    create about and error page components

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/AboutPage";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <Wrapper>
+      <h3>About Us</h3>
+      <p>
+        Introducing "MixDrinks," a sleek cocktail app revolutionizing mixology.
+        Access a vast database of recipes via our API, ensuring real-time
+        updates and an extensive collection of concoctions. Discover
+        personalized recommendations based on your preferences, and easily
+        create shopping lists for ingredients. With interactive features like
+        virtual mixing tutorials and a social platform to share your favorite
+        recipes, MixMaster brings the art of cocktail crafting to your
+        fingertips. Elevate your home bar experience with this innovative,
+        API-powered app. Cheers to endless possibilities in every sip!
+      </p>
+    </Wrapper>
   );
 };
 
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -3,10 +3,12 @@ import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
   return (
-    <div>
+    <>
       <Navbar />
-      <Outlet />
-    </div>
+      <section className="page">
+        <Outlet />
+      </section>
+    </>
   );
 };
 
