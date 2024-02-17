c1c46ebe7e105ca7687b7ca7c06726ac84f92cf2
radu-bordea <radu.bordea.dev@gmail.com>
2024-02-17 19:08:26 +0100

    fecth data to Cocktail page

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,4 +1,9 @@
-import { useLoaderData, Link } from "react-router-dom";
+import {
+  useLoaderData,
+  useNavigate,
+  Link,
+  Navigate,
+} from "react-router-dom";
 import axios from "axios";
 import Wrapper from "../assets/wrappers/CocktailPage";
 const singleCocktailUrl =
@@ -6,13 +11,83 @@ const singleCocktailUrl =
 
 export const loader = async ({ params }) => {
   const { id } = params;
-  const data = await axios.get(`${singleCocktailUrl}${id}`);
+  const { data } = await axios.get(`${singleCocktailUrl}${id}`);
+
   return { id, data };
 };
 
 const Cocktail = () => {
   const { id, data } = useLoaderData();
-  return <h1>Cocktail</h1>;
-};
 
+  // if (!data) return <h2>something went wrong...</h2>;
+  if (!data) return <Navigate to="/" />;
+
+  const singleDrink = data.drinks[0];
+
+  const {
+    strDrink: name,
+    strDrinkThumb: image,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions,
+  } = singleDrink;
+
+  const validIngredients = Object.keys(singleDrink)
+    .filter(
+      (key) => key.startsWith("strIngredient") && singleDrink[key] !== null
+    )
+    .map((key) => singleDrink[key]);
+
+  console.log(validIngredients);
+
+  console.log(singleDrink);
+
+  return (
+    <Wrapper>
+      <header>
+        <Link to="/" className="btn">
+          back homne
+        </Link>
+        <h3>{name}</h3>
+      </header>
+      <div className="drink">
+        <img src={image} alt={name} className="img" />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">name :</span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">category :</span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">info :</span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">glass :</span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">ingredients :</span>
+            {validIngredients.map((item, index) => {
+              return (
+                <span className="ing" key={index}>
+                  {item}
+                  {index < validIngredients.length - 1 ? "," : ""}
+                </span>
+              );
+            })}
+          </p>
+          <p>
+            <span className="drink-data">instructions :</span>
+            {instructions}
+          </p>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 export default Cocktail;
